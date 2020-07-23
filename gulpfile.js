fe4c7bc3c5b950dd4c40e33f848e67ba3298d735
Maria Popleteeva <m.popleteeva@gmail.com>
2020-07-23 22:30:36 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,67 @@
 // Include gulp
-var gulp = require('gulp'),
+var gulp = require("gulp"),
   // Include Our Plugins
-  less = require('gulp-less'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+  less = require("gulp-less"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  imagemin = require("gulp-imagemin"),
+  plumber = require("gulp-plumber"),
+  sourcemap = require("gulp-sourcemaps"),
+  server = require("browser-sync").create();
 
-gulp.task('css', function () {
+gulp.task("css", function () {
   gulp
-    .src('./less/main.less')
-    .pipe(less({ strictMath: true }))
-    .pipe(postcss([
-      autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
-    ]))
-    .pipe(gulp.dest('./css'));
+    .src("./less/main.less")
+    .pipe(plumber())
+    .pipe(gulpIf(sourcemap.init()))
+    .pipe(
+      less({
+        strictMath: true,
+      })
+    )
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: ["> 1%", "IE 9", "IE 10"],
+        }),
+      ])
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(server.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less', ['css']);
+gulp.task("image-min", () => {
+  return gulp
+    .src("./img/**/*.{svg,jpg,png}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({
+          optimizationLevel: 3,
+        }),
+      ])
+    )
+    .pipe(gulp.dest("img/"));
 });
 
-gulp.task('default', ['css', 'watch']);
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false,
+    index: "index.html",
+  });
+});
+
+gulp.task("watch", function () {
+  gulp.watch("./less/**/*.less", ["css"]);
+  gulp.watch("./*.html", ["refresh"]);
+});
+
+gulp.task("default", ("css", ["watch", "server"]));
